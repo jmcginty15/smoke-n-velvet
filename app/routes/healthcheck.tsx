524debc5c41b1c5864d257bcb4244437a13f3e61
Jason McGinty <jasonmcginty@Jasons-MacBook-Air.local>
2024-05-23 17:07:42 -0500

    remove user check from healthcheck route

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,7 +1,7 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderFunctionArgs } from "@remix-run/node";
 
-import { prisma } from "~/db.server";
+// import { prisma } from "~/db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const host =
@@ -12,7 +12,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
-      prisma.user.count(),
+      // prisma.user.count(),
       fetch(url.toString(), { method: "HEAD" }).then((r) => {
         if (!r.ok) return Promise.reject(r);
       }),
